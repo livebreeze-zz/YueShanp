fb80264c32dcad4685e7043bce9d591577642b4b
SeanChen <livebreeze@gmail.com>
2016-03-08 21:53:06 +0800

    add @font-face "Noto Sans TC"

--- YueShanp/App_Start/BundleConfig.cs
+++ YueShanp/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace YueShanp
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Fonts/notosanstc.css"));
         }
     }
 }
