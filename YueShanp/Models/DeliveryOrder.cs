381b92c3a04adc98cacae58eec1d3ab6cc23a5dd
SeanChen <livebreeze@gmail.com>
2016-04-23 00:35:48 +0800

    add DeliveryOrder Models

---
+++ YueShanp/Models/DeliveryOrder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace YueShanp.Models
+{
+    public class DeliveryOrder : BaseEntity<int>
+    {
+        [Required]
+        public DateTime DeliveryDate { get; set; }
+
+        [Required]
+        [StringLength(6)]
+        public int ReceivableMoth { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+        public virtual List<DeliveryOrderDetail> DeliveryOrderDetailList { get; set; }
+
+    }
+}
---
+++ YueShanp/Models/DeliveryOrderDetail.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace YueShanp.Models
+{
+    public class DeliveryOrderDetail : BaseEntity<int>
+    {
+        [Required]
+        [DisplayName("數量")]
+        public int Qty { get; set; }
+
+        [Required]
+        [DisplayName("單價")]
+        public decimal UnitPrice { get; set; }
+
+        public virtual Product Product { get; set; }
+    }
+}
