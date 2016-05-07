2f4b746acde454393bfad5c7fbe2a5690396edf0
SeanChen <livebreeze@gmail.com>
2016-05-07 22:31:09 +0800

    add angular bundles

--- YueShanp/App_Start/BundleConfig.cs
+++ YueShanp/App_Start/BundleConfig.cs
@@ -9,10 +9,13 @@ namespace YueShanp
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js"));           
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+               "~/Scripts/angular.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -21,7 +24,9 @@ namespace YueShanp
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/angular-ui/ui-bootstrap.min.js",
+                      "~/Scripts/angular-ui/ui-bootstrap-tpls.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
